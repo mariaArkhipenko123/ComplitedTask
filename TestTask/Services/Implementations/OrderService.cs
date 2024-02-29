73fa3a7cafafe0976132e3a66644cf036a1479da
Мария Архипенко <119293452+mariaArkhipenko123@users.noreply.github.com>
2024-02-29 20:01:03 +0300

    Добавлен файл .vsidx

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class OrderService : IOrderService
+    {
+        private readonly ApplicationDbContext _order;
+
+        public OrderService(ApplicationDbContext order)
+        {
+            _order = order;
+        }
+
+        public async Task<Order> GetOrder()
+        {
+            return await _order.Orders.FirstOrDefaultAsync();
+        }
+
+        public async Task<IList<Order>> GetOrders()
+        {
+            return await _order.Orders.ToListAsync();
+        }
+
+        public Order GetOrderWithHighestTotalAmount()
+        {
+            var orderWithHighestTotalAmount = _order.Orders
+                .OrderByDescending(o => o.Price * o.Quantity)
+                .FirstOrDefault();
+
+            return orderWithHighestTotalAmount;
+        }
+
+        public List<Order> GetOrdersWithQuantityGreaterThan(int quantity)
+        {
+            var ordersWithQuantityGreaterThan = _order.Orders
+                .Where(o => o.Quantity > quantity)
+                .ToList();
+
+            return ordersWithQuantityGreaterThan;
+        }
+
+    }
+}
---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Implementations;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+
+        private readonly ApplicationDbContext _user;
+
+        public UserService(ApplicationDbContext user)
+        {
+            _user = user;
+        }
+        public async Task<User> GetUser()
+        {
+            return await _user.Users.FirstOrDefaultAsync();
+        }
+
+        public async Task<IList<User>> GetUsers()
+        {
+            return await _user.Users.ToListAsync();
+        }
+
+        public User GetUserWithMostOrders()
+        {
+            var userWithMostOrders = _user.Users
+                .OrderByDescending(u => u.Orders.Count)
+                .FirstOrDefault();
+
+            return userWithMostOrders;
+        }
+
+        public List<User> GetInactiveUsers()
+        {
+            var inactiveUsers = _user.Users
+                .Where(u => u.Status == UserStatus.Inactive)
+                .ToList();
+
+            return inactiveUsers;
+        }
+    }
+}
